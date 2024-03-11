a326e6ae82467485770d35f4fa4a956a3bca79ab
Blackie360 <felixkent360@gmail.com>
2024-03-11 14:56:45 +0300

    login screen

---
+++ app/__layout.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function __layout() {
+  return (
+    <View>
+      <Text>__layout</Text>
+    </View>
+  )
+}
--- app/index.jsx
+++ app/index.jsx
@@ -1,8 +1,26 @@
 import { View, Text, Button } from 'react-native'
-import React from 'react'
-import {Link} from 'expo-router'
+import React, { useEffect } from 'react'
+import {Link, useRouter} from 'expo-router'
+import services from '../utils/services'
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    checkUserAuth();
+  }, []);
+
+  const checkUserAuth=async ()=>{
+    const result = await  services.getData("login");
+    console.log("result",result);
+    if(result !== 'true')
+    {
+      router.replace('/login');
+    }
+
+  }
+
   return (
     <View style={{
         marginTop: 20,
